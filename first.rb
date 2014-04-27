f4a84094ba06baee5c68be5e532d4b8c2dd72051
Harsh Chaudhary <harsh@Harshs-MacBook-Pro.local>
2014-04-26 23:43:36 -0400

    Changed folders

--- first.rb
+++ first.rb
@@ -1,6 +1,6 @@
-c_value = File.read("temp.dat").to_i
+c_value = File.read("in/temp.dat").to_i
 f_value = (c_value * 9/5) + 32
-fh = File.new("op.txt", "w")
+fh = File.new("op/op.txt", "w")
 fh.puts f_value
 fh.close
 
